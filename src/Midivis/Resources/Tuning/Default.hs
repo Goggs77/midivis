4162560d8e14166265ca7fb8beb8e3cc23a33629
Goggs <the801140@outlook.com>
2026-07-24 23:06:11 +0800

    update instance binding

--- src/Midivis/Resources/Tuning/Default.hs
+++ src/Midivis/Resources/Tuning/Default.hs
@@ -1,41 +1,53 @@
+{-# LANGUAGE InstanceSigs #-}
 module Midivis.Resources.Tuning.Default where
+import Midivis.Tuning.TuningParser ( Tuning(..) )
 -- 12 EDO, or 12 TET
 newtype EDO12 = EDO12 {lowestFreq :: Double}
-calibreate :: Integral b => EDO12 -> b -> Double -> EDO12
-calibreate (EDO12 _) note freq = --this equation: newlow*2**(note/12) == freq
-    let
-        newlow = freq/(2.0**(fromIntegral note/12.0))
-    in EDO12 newlow
+instance Tuning EDO12 where
 
-calibreateA4 :: EDO12 -> Double -> EDO12
-calibreateA4 (EDO12 l) = calibreate (EDO12 l) 69
+    period :: EDO12 -> Integer
+    period _ = 12
 
-calibreateC4 :: EDO12 -> Double -> EDO12
-calibreateC4 (EDO12 l) = calibreate (EDO12 l) 60
+    lowestFreq :: EDO12 -> Double
+    lowestFreq (EDO12 l) = l
 
-getFreq :: Integral a => EDO12 -> a -> Double
-getFreq (EDO12 l) note = l*(2.0**(fromIntegral note/12.0))
+    calibrate :: Integral b => EDO12 -> b -> Double -> EDO12
+    calibrate (EDO12 _) note freq = --this equation: newlow*2**(note/12) == freq
+        let
+            newlow = freq/(2.0**(fromIntegral note/12.0))
+        in EDO12 newlow
+
+    calibrateA4 :: EDO12 -> Double -> EDO12
+    calibrateA4 (EDO12 l) = calibrate (EDO12 l) 69
+
+    calibrateC4 :: EDO12 -> Double -> EDO12
+    calibrateC4 (EDO12 l) = calibrate (EDO12 l) 60
+
+    getFreq :: Integral a => EDO12 -> a -> Double
+    getFreq (EDO12 l) note = l*(2.0**(fromIntegral note/12.0))
+
+    
+    getGeneralName :: Integral a => EDO12 -> a -> String
+    getGeneralName _ note =
+        let
+            (q, r) = fromIntegral note `quotRem` 12
+            name = case r of
+                0 -> "C"
+                1 -> "C#"
+                2 -> "D"
+                3 -> "D#"
+                4 -> "E"
+                5 -> "F"
+                6 -> "F#"
+                7 -> "G"
+                8 -> "G#"
+                9 -> "A"
+                10 -> "A#"
+                11 -> "B"
+                _ -> error "There must have been a program messing with memory, otherwise a cosmic ray hit your computer!"
+        in
+            name ++ show q
 
 --get the higher tone over the lower tone, over a range of some semitones
 getRatio :: Integral a => EDO12 -> a -> Double 
 getRatio (EDO12 _) diff = 2.0**(fromIntegral diff/12.0)
-
-getGeneralName :: Integral a => a -> String
-getGeneralName note =
-    let
-        (q, r) = fromIntegral note `quotRem` 12
-        name = case r of
-            0 -> "C"
-            1 -> "C#"
-            2 -> "D"
-            3 -> "D#"
-            4 -> "E"
-            5 -> "F"
-            6 -> "F#"
-            7 -> "G"
-            8 -> "G#"
-            9 -> "A"
-            10 -> "A#"
-            11 -> "B"
-            _ -> error "There must have been a program messing with memory, otherwise a cosmic ray hit your computer!"
-    in name ++ show q
--- src/Midivis/Resources/Tuning/EDO.hs
+++ src/Midivis/Resources/Tuning/EDO.hs
@@ -1,5 +1,6 @@
+{-# LANGUAGE InstanceSigs #-}
 module Midivis.Resources.Tuning.EDO where
-import Midivis.Tuning.TuningParser (Tuning)
+import Midivis.Tuning.TuningParser ( Tuning(..) )
 
 data EDO = EDO
     {
@@ -7,27 +8,34 @@ data EDO = EDO
         lowestFreq :: Double --frequency for C0 (the note 0); negative frequency is phase negation
     } 
 instance Tuning EDO where
+
+    period :: EDO -> Integer
+    period (EDO den _) = den
+
+    lowestFreq :: EDO -> Double
+    lowestFreq (EDO _ l) = l
+    
 -- given a EDO, define a frequency for a specific note(Int ID, e.g. C4 == 60), and returun the callibrated EDO
-calibreate ::Integral a => EDO -> a -> Double -> EDO
-calibreate (EDO den _) note freq = --this equation: newlow*2**(note/den) == freq
-    let
-        newlow = freq/(2.0**(fromIntegral note/fromIntegral den))
-    in EDO den newlow
+    calibrate ::Integral a => EDO -> a -> Double -> EDO
+    calibrate (EDO den _) note freq = --this equation: newlow*2**(note/den) == freq
+        let
+            newlow = freq/(2.0**(fromIntegral note/fromIntegral den))
+        in EDO den newlow
 
-calibreateA4 :: EDO -> Double -> EDO
-calibreateA4 e = calibreate e 69
+    calibrateA4 :: EDO -> Double -> EDO
+    calibrateA4 e = calibrate e 69
 
-calibreateC4 :: EDO -> Double -> EDO
-calibreateC4 e = calibreate e 60
+    calibrateC4 :: EDO -> Double -> EDO
+    calibrateC4 e = calibrate e 60
 
-getFreq :: Integral a => EDO -> a -> Double
-getFreq (EDO den l) note = l*(2.0**(fromIntegral note/fromIntegral den))
+    getFreq :: Integral a => EDO -> a -> Double
+    getFreq (EDO den l) note = l*(2.0**(fromIntegral note/fromIntegral den))
+
+    getGeneralName :: Integral a => EDO -> a -> String
+    getGeneralName (EDO den _) note =
+          let (q, r) = fromIntegral note `quotRem` fromIntegral den
+          in show r ++ "/" ++ show q --allow q=0
 
 --get the higher tone over the lower tone, over a range of some semitones
 getRatio :: Integral a => EDO -> a -> Double 
 getRatio (EDO den _) diff = 2.0**(fromIntegral diff/ fromIntegral den)
-
-getGeneralName :: Integral a => EDO -> a -> String
-getGeneralName (EDO den _) note =
-      let (q, r) = fromIntegral note `quotRem` fromIntegral den
-      in show r ++ "/" ++ show q --allow q=0
