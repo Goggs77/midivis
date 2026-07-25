628a8082229422ab07a987824f1ac8fab13076ff
Goggs <the801140@outlook.com>
2026-07-25 19:22:26 +0800

    switch to gloss-relative

--- src/Midivis/Render/Example.hs
+++ src/Midivis/Render/Example.hs
@@ -1,4 +1,19 @@
 module Midivis.Render.Example where
-import Apecs.Gloss
+
+import Graphics.Gloss.Relative
+
+import Midivis.World
+
+rgba :: Float -> Float -> Float -> Float -> Color
+rgba = makeColor
+rgbaI :: Int -> Int -> Int -> Int -> Color
+rgbaI = makeColorI
+
 drawExampleText :: Picture
-drawExampleText =  (color white) (Text "This is an example window")
+drawExampleText =  (scale 0.1 0.1 . color white) (Text "This is an example window")
+
+drawRelativeFrame :: World -> IO Frame
+drawRelativeFrame w = return $ 
+    
+    zoom (Relative 0.5) (Relative 0.5) alignCenter $ 
+        thickEllipse (rgba 1 1 1 1) (Absolute 5.0)
