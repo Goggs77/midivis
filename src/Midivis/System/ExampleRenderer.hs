1ff95b33be427308bf267a35165b94bbd25c337e
Goggs <the801140@outlook.com>
2026-07-25 22:41:52 +0800

    midi buffer management

--- src/Midivis/System/ExampleRenderer.hs
+++ src/Midivis/System/ExampleRenderer.hs
@@ -9,20 +9,18 @@ import Sound.RtMidi (InputDevice)
 
 
 drawExampleRelative :: World -> IO ()
-drawExampleRelative w = do
+drawExampleRelative w0 = do
     inputDevice <- initMidi
     putStrLn "[Debug] Midi Initialized"
     playRelativeIO
         (InWindow "Example Relative Window" (800,600) (100,100))
         black
         1000
-        w
+        w0
         drawRelativeFrame
         handleEvent
         (step inputDevice)
 
 step :: InputDevice -> Float -> World -> IO World
 step inputDevice dT w0 = do
-    {-putStrLn $ "[Debug] Stepping"-}
-    bufferMidi inputDevice w0 -- add midi event to buffer
-    {-putStrLn $ "[Debug] Clearing buffer"-}
+    bufferMidi inputDevice w0 >>= cleanBuffer 
--- src/Midivis/System/MidiQuery.hs
+++ src/Midivis/System/MidiQuery.hs
@@ -3,20 +3,19 @@ module Midivis.System.MidiQuery where
 
 import qualified Data.Vector.Storable as V
 import Data.Vector.Storable (Vector)
-import Data.Word
+
 import Sound.RtMidi
-import Control.Concurrent.Chan (Chan, readChan, writeChan)
-import Control.Concurrent (forkIO)
 
 import Midivis.Midi.MidiParser
+import Midivis.Midi.MidiEventType
 import Midivis.World
 
 -- Helpers to manipulate the global MidiEventBuffer
 appendMidiEvent :: MidiEvent -> World -> IO World
-appendMidiEvent e w0 = return w0 {midiEvtBuf = midiEvtBuf w0 <> MidiEventBuffer (V.fromList [e])}
+appendMidiEvent e w0 = return w0 {midiEvtBuf = midiEvtBuf w0 <> pack (V.fromList [e])}
 
 appendMidiEvents :: Vector MidiEvent -> World -> IO World
-appendMidiEvents vs w0 = return w0 {midiEvtBuf = midiEvtBuf w0 <> (MidiEventBuffer vs)}
+appendMidiEvents vs w0 = return w0 {midiEvtBuf = midiEvtBuf w0 <> pack vs}
 
 getMidiBuffer :: World -> MidiEventBuffer
 getMidiBuffer w0 = midiEvtBuf w0
@@ -50,4 +49,26 @@ bufferMidi inputDevice w0 = do
     let mEvt = parse msg
     case mEvt of
         Nothing -> {-(putStrLn "[Debug] Nothing to append") >>-} return w0
-        Just e -> (putStrLn $ "[Debug] Appended MidiEvent" ++ show e) >> appendMidiEvent e w0
+        Just e  -> {-(putStrLn $ "[Debug] Appended " ++ show e) >>-} appendMidiEvent e w0
+
+cleanBuffer :: World -> IO World
+cleanBuffer w0 = do
+    let buf = V.toList (unpack $ midiEvtBuf w0)
+        noteOns  = filter (\e -> evt e == NoteOn) buf
+        noteOffs = filter (\e -> evt e == NoteOff) buf
+        processedOns = dropSameId noteOffs noteOns
+        -- all NoteOffs must be eliminated, so no need to process, as the error below
+    {-putStrLn $ "[Debug] Original Buffer: " ++ show buf
+    putStrLn $ "[Debug] Original Ons: " ++ show noteOns
+    putStrLn $ "[Debug] Original Offs: " ++ show noteOffs-}
+    if (length noteOns >= length noteOffs) 
+        then
+            putStrLn ("[Debug] Cleaned Buffer: " ++ show processedOns) >>
+            return w0{midiEvtBuf = pack $ V.fromList processedOns} 
+        else error "Note On/Off mismatch"
+    where
+        dropSameId [] ys = ys
+        dropSameId (x:xs) ys = 
+            -- drop the first sameId element
+            let newYs = (takeWhile (sameValueL x) ys) ++ (drop 1 $ dropWhile (sameValueL x) ys)
+            in dropSameId xs newYs
