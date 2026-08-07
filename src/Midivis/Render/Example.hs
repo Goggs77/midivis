9f4e09565ceb9d9781cb5a2af5845f72f45b8132
Goggs <the801140@outlook.com>
2026-08-07 21:56:56 +0800

    UI rework

--- src/Midivis/Render/Example.hs
+++ src/Midivis/Render/Example.hs
@@ -1,15 +1,20 @@
 module Midivis.Render.Example where
 import qualified Data.Vector.Storable as V
 import Data.Vector.Storable ( toList )
+import qualified Data.StorableVector as SV
 import Graphics.Gloss.Relative
 import Data.Tuple.Extra (uncurry3)
+import qualified Data.Text as T
 
-import Midivis.Tuning.Scala
+import Midivis.Tuning.Ascl 
 import Midivis.Midi.MidiParser
 import Midivis.Util.Math
 import Midivis.World
+import Midivis.Synth.Types
+import Midivis.Synth.VoicePool
 import Midivis.Midi.MidiEventType (MidiEventType(..))
-
+import Midivis.Render.Text (utf8Banner, TextSize(..), titleSize, subtitleSize, bodySize, TextFill(..))
+import Midivis.Synth.CtrlBus
 
 rgba :: Float -> Float -> Float -> Float -> Color
 rgba = makeColor
@@ -25,43 +30,50 @@ drawRelativeFrame w0 = do
         bottomFrame = Aspect (1, 0.73) alignBottom . debugAddBorder .
             zoom (Relative 0.8) (Relative 0.104) (alignAt (Relative 0, Relative (-1))) . debugAddBorder .
             zoom (Relative 1) (Relative 0.333) alignCenter
+    notes <- mapToRadii w0 (rgba 1 1 1 0.95)
     let centerPoint = centerFrame $
             solidEllipse (rgba 1 1 1 0.05)
         movingLine = centerFrame $
-            mapToRadii w0 (rgba 1 1 1 0.95)
+            notes
         middleCircle = centerFrame $
             thickEllipse (rgba 1 1 1 0.8) (Absolute 2.0)
+            <> solidEllipse ((realToFrac $ bufferTransparency w0) `withAlpha` white)
         bottomText = bottomFrame $
-            banner (name $ sclOfChoice w0) (rgba 1 1 1 0.95)
+            utf8Banner bodySize FillFit alignCenter (T.unpack $ tunName $ sclOfChoice w0) (rgba 1 1 1 0.95)
     return $ Overlay [movingLine, centerPoint, middleCircle, bottomText]
 
 -- pl(radius) = radii
-mapToRadii :: World -> Color -> Frame
-mapToRadii w0 clr = 
+mapToRadii :: World -> Color -> IO Frame
+mapToRadii w0 clr = do
     let evts = unpack $ midiEvtBuf w0
-        freqs = toList $ V.map (getFreq (sclOfChoice w0)) (V.map valueL (V.filter (\e -> evt e == NoteOn) evts))
+        noteOns = V.filter (\e -> evt e == NoteOn) evts
+        freqs = toList $ V.map (getFreq (sclOfChoice w0)) (V.map valueL noteOns)
         names = map
-            (getGeneralName (sclOfChoice w0) . valueL)
+            (T.unpack . getGeneralName (sclOfChoice w0) . valueL)
             (toList (V.filter (\ e -> evt e == NoteOn) evts))
-        phaseDiff = 2*pi/fromIntegral (length freqs)
-        aPh = zip (take (length freqs) (iterate (+phaseDiff) (baseAngle w0))) (replicate (length freqs) phaseDiff)
+        -- phaseDiff = 2*pi/fromIntegral (length freqs)
+        aPh = map (\ x -> 2 * pi * log2 (x / freqA4 w0) + baseAngle w0) freqs
+    amps <- V.toList <$> V.mapM (\ x -> readAmp (voicePool w0) (valueL x)) noteOns
+    mw <- ccNorm (ctrlBus w0) 1
+    let lengths = map (scaleLog2 20 18000 0 1) freqs
+        radii = zipWith (\ x y -> (x * 0.35 + 0.45 + 0.20 * mw) * y) amps lengths
         -- zip for processing
-        zipped = zip3 aPh (scaleNumLog2 20 18000 0 1 freqs) names 
-    in Overlay $ map (uncurry3 (customRadius clr)) zipped
+    let zipped = zip3 aPh radii names 
+    return $ Overlay $ map (uncurry3 (customRadius clr)) zipped
 
 
 -- | Creates a radius with color, angle, radius
-customRadius :: Color -> (Double, Double) -> Double -> String -> Frame
-customRadius clr (angle, phaseDiff) radius na  =
-    zoom (Relative 0.05) (Relative 0.05) (alignAt (p2 .* 1.2)) (banner na clr) 
-    <> aspect (1,1) alignCenter (zoom (Relative 1) (Relative 1) (alignAt (p2 .* 1.2)) (solidEllipse (0.05 `withAlpha` clr))) 
+customRadius :: Color -> Double -> Double -> String -> Frame
+customRadius clr angle radius na  = 
+    (zoom (Relative 0.25) (Relative 0.025) (alignAt (p2 .* 1.2)) . debugAddBorder) (utf8Banner subtitleSize FillFit alignCenter na clr) 
+    <> aspect (1,1) alignCenter (zoom (Relative 0.20) (Relative 0.20) (alignAt (p2 .* 1.2)) (solidEllipse (0.033 `withAlpha` clr))) 
     <> stroke [zeroRelative, p2 .* 0.86] clr 
-    <> fit (color clr $ arc (realToFrac $ rad2deg angle) (realToFrac $ rad2deg $ angle + phaseDiff) (realToFrac $ 0.43*radius))
+    -- <> fit (color clr $ arc (realToFrac $ rad2deg angle) (realToFrac $ rad2deg $ angle + phaseDiff) (realToFrac $ 0.43*radius))
     where p2 = (Relative $ realToFrac.(0.5*radius*).cos $ angle, Relative $ realToFrac.(0.5*radius*).sin $ angle)
     
 
 debugAddBorders :: [Frame] -> [Frame]
-debugAddBorders = map (border (Absolute 2) (rgba 0 1 0 0.9))
+debugAddBorders = map debugAddBorder
 
 debugAddBorder :: Frame -> Frame
 --debugAddBorder f = border (Absolute 2) (rgba 0 1 0 0.9) f -- delete comments for debug
